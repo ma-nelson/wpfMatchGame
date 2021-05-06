55e5f1a02e8522dc6ca1006b122729a97717a517
ma-nelson <74280834+ma-nelson@users.noreply.github.com>
2021-05-06 10:25:57 -0600

    Add files via upload

---
+++ WpfMatchgame/Controls/Card.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WpfMatchgame.Controls
+{
+  
+    public partial class Card : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public enum eState
+        {
+            Inactive,
+            Idle,
+            Flipped,
+            Matched
+        }
+        public Card()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            this.Loaded += Card_Loaded;
+        }
+        public GameWindow Owner { get; set; }
+        private eState state { get; set; } = eState.Inactive;
+
+
+        public eState State {
+            get { return state; }
+            set
+            {
+                if(value != state)
+                {
+                    state = value;
+                    Interactable = (state == eState.Idle);
+                    Show = (state == eState.Flipped || state == eState.Matched);
+                    NotifyPropertyChanged("State");
+
+                }
+            }
+        }
+
+        public bool Show {
+            set
+            {
+                if (value)
+                {
+                    lblSymbol.Visibility = Visibility.Visible;
+                    imgCard.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    lblSymbol.Visibility = Visibility.Hidden;
+                    imgCard.Visibility = Visibility.Visible;
+                }
+
+            }
+        }
+
+
+        private bool interactable;
+
+        public bool Interactable
+        {
+            get { return interactable; }
+            set
+            {
+                if (value != interactable)
+                {
+                    interactable = value;
+                    NotifyPropertyChanged("Interactable");
+                }
+            }
+
+        }
+
+        private string symbol;
+
+        public string Symbol { get { return symbol; }
+            set
+             {
+                if (value != Symbol)
+                {
+                    symbol = value;
+                    NotifyPropertyChanged("Symbol");
+                }
+            }
+        }
+        private void Card_Loaded(object sender, RoutedEventArgs e) 
+        {
+            Owner = (GameWindow)Window.GetWindow(this);
+            Owner.RegisterCard(this);
+        }
+
+        private void btnCard_Click(object sender, RoutedEventArgs e)
+        {
+            State = eState.Flipped;
+            Owner.SelectCard(this);
+        }
+    }
+}
---
+++ WpfMatchgame/GameWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Threading;
+using WpfMatchgame.Controls;
+
+namespace WpfMatchgame
+{
+    
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += TimerTick;
+        }
+        public List<Card> cards { get; set; } = new List<Card>();
+        private Random random = new Random();
+        private DispatcherTimer timer = new DispatcherTimer();
+
+        private List<string> symbols = new List<string>()
+            {
+            "!", "!", "N", "N", ",", ",",
+            "b", "b", "v", "v", "w", "w",
+        };
+
+        private Card card1 = null;
+        private Card card2 = null;
+
+        public void RegisterCard(Card card)
+        {
+            card.State = Card.eState.Idle;
+            int r = random.Next(symbols.Count);
+            card.Symbol = symbols[r];
+
+            symbols.RemoveAt(r);
+            cards.Add(card);
+        }
+
+        public void SelectCard(Card card)
+        {
+            if (card1 == null)
+            {
+                card1 = card;
+            }
+            else
+            {
+                if (card2 == null)
+                {
+                    card2 = card;
+
+                    if (card1.Symbol == card2.Symbol)
+                    {
+                        card1.State = Card.eState.Matched;
+                        card2.State = Card.eState.Matched;
+                        card1 = card2 = null;
+                    }
+                    else
+                    {
+                        
+                        foreach (Card c in cards)
+                        {
+                            if (c.State == Card.eState.Idle)
+                            {
+                                c.State = Card.eState.Inactive;
+                            }
+                        }
+                        timer.Start();
+                    }
+                }
+            }
+
+        }
+      
+        private void TimerTick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            foreach (Card c in cards)
+            {
+                if (c.State != Card.eState.Matched)
+                {
+                    c.State = Card.eState.Idle;
+                }
+            }
+            card1 = card2 = null;
+        }
+    }
+}
